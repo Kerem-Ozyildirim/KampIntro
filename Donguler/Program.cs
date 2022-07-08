807968a3b4f51013de732f4080f3341031464b39
tesla <tesla@DESKTOP-M3QHSV1>
2022-07-09 00:55:31 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,30 @@
+﻿string kurs1 = "Yazılım Gelitirici Yetirtirme Kamp";
+string kurs2 = "Programalamaya başlangıç için temel kurs";
+string kurs3 = "Java";
+string kurs4 = "Python";
+string kurs5 = "C++";
+string kurs6= "C#";
+
+Console.WriteLine(kurs1);
+Console.WriteLine(kurs2);
+Console.WriteLine(kurs3);
+Console.WriteLine(kurs4);
+Console.WriteLine(kurs5);
+Console.WriteLine(kurs6);
+
+//array - Dizi
+
+string[] kurslar = new string[] { "Yazılım Gelitirici Yetirtirme Kamp", 
+    "Programalamaya başlangıç için temel kurs", "Java", "Python", "C++", "C#" };
+
+for (int i = 0; i< kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i]);
+}
+
+foreach (string kurs in kurslar)
+{
+    Console.WriteLine(kurs);
+}
+
+Console.WriteLine("Sayfa sonu - footer");
---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //Syntax
+        public void Ekle1(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, double stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi + " : " + aciklama + " : "  + fiyat + " : " + stokAdedi);
+        }
+    }
+}
